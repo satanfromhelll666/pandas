d319def24273b5bd63b36b96a5fa7fb096fb33b7
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-02-01 14:06:13 +0600

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,1 +1,11 @@
+pk.sort_values(['Name'], ascending=True) # sort all data ascending order
 
+pk.sort_values(['Name'], ascending=False)#sort all data desending order
+
+# we can take maltuiple value
+
+pk.sort_values(['Name','HP'],ascending = [1,0]) #1 = true and 0 = false
+
+pk['Total'] = pk['HP'] + pk['Attack'] + pk['Defense'] + pk['Sp. Atk'] + pk['Sp. Def'] + pk['Speed']
+
+pk.head(5)#create a columnb named Total all the sum of parameter 
