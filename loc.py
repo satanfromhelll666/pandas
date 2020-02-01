e376455ecaea1a8927a5ce061c910617705bb109
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-02-01 14:34:44 +0600

    Update loc.py

--- loc.py
+++ loc.py
@@ -1,3 +1,5 @@
 pk.loc[pk['Type 1'] == 'Fire'] # print all the Fire pokemon on the type 1 coloum
 
-pk.loc[(pk['Type 1']=='Grass') & (pk['Type 2']=='Ice')]
+pk.loc[(pk['Type 1']=='Grass') & (pk['Type 2']=='Ice')] # and oparetion
+
+pk.loc[(pk['Type 1']=='Grass') | (pk['Type 2']=='Ice')] # or oparation
