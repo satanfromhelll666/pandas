c9ec48119fd7d5901bc7e3181ba86cf3d3ac54de
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-02-01 15:05:10 +0600

    Update loc.py

--- loc.py
+++ loc.py
@@ -5,3 +5,7 @@ pk.loc[(pk['Type 1']=='Grass') & (pk['Type 2']=='Ice')] # and oparetion
 pk.loc[(pk['Type 1']=='Grass') | (pk['Type 2']=='Ice')] # or oparation
 
 pk.loc[(pk['Type 1']=='Grass') | (pk['Type 2']=='Ice') & (pk["HP"] >70 ) ]
+
+pk.loc[pk['Name'].str.contains('Mega')] # it print all the name of pokemon who contain mega name.
+
+pk.loc[~pk['Name'].str.contains('Mega')] # it will print all the name of pokemon which not contain mega name
