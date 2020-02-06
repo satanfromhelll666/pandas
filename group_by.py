df8dfdd62e818a3d10723031fdd337e660bbc0e8
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-02-06 16:12:27 +0600

    Update group_by.py

--- group_by.py
+++ group_by.py
@@ -5,3 +5,14 @@ pk.groupby(['Type 1']).mean().sort_values('Defense') #it will sort the value acc
 pk.groupby(['Type 1']).mean().sort_values('Sp. Atk' , ascending = False) # it will show the name of the type 1 pokemon which has the highest sp.Atk
 
 pk.groupby(['Type 1']).sum() #it will sum the all numeric coloum 
+
+pk.groupby(['Type 1' , 'Type 2']).count() # here type 1 is bug and type 2 is electric....etc
+
+'''
+Bug	
+  Electric	2	2	2	2	2	2	2	2	2	2
+  Fighting	2	2	2	2	2	2	2	2	2	2
+  Fire	2	2	2	2	2	2	2	2	2	2
+  Flying	14	14	14	14	14	14	14	14	14	14
+  Ghost	1	1	1	1	1	1	1	1	1	1
+'''
