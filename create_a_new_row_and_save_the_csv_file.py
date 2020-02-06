265365f7b4e9cf5a1831777c37504a039155d811
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-02-06 15:50:45 +0600

    Update create_a_new_row_and_save_the_csv_file.py

--- create_a_new_row_and_save_the_csv_file.py
+++ create_a_new_row_and_save_the_csv_file.py
@@ -1,5 +1,7 @@
 pk['Total'] = pk['HP'] + pk['Attack'] + pk['Defense'] + pk['Sp. Atk'] + pk['Sp. Def'] + pk['Speed']
 
+pk['new'] = 1 # it will also create a new columb
+
 pk.head(5)#create a columnb named Total all the sum of parameter 
 
 pk['Total'] = pk.iloc[:,4:10].sum(axis=1) #here add a total columb total add 4 to 9 columnb and axis = 1 means create a columnb if it axis =  0 it will create a row
